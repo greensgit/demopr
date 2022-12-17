950c3fc17a42e168c2a2f0e69c7b08a68449008c
greensgit <rajgreensomr@gmail.com>
2022-12-17 14:48:37 +0530

    new code

---
+++ src/org/tcs/test/ScannerClass.java
@@ -0,0 +1,53 @@
+package org.tcs.test;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+	
+	public static void main(String[] args) {
+		//Scanner sck = new Scanner(System.in);
+
+		Scanner sc = new Scanner(System.in);
+		
+		//ctrl+2 and then release and press L
+		System.out.println("Enter a byte value as input");
+		byte nextByte = sc.nextByte();
+		System.out.println("Output byte value is" +nextByte);
+		
+		byte nextBytee = sc.nextByte();
+		System.out.println("Output byte value is" +nextBytee);
+
+		System.out.println("Enter short value as input");
+		short nextShort = sc.nextShort();
+		System.out.println("Output short value is"+nextShort);
+		
+		System.out.println("Enter integer value as input");
+		int nextInt = sc.nextInt();
+		System.out.println("Output integer value is"+nextInt);
+		
+		System.out.println("Enter long value as input");
+		long nextLong = sc.nextLong();
+		System.out.println("Output long value is"+nextLong);
+		
+		System.out.println("Enter floar value as input");
+		float nextFloat = sc.nextFloat();
+		System.out.println("Output float value is"+nextFloat);
+		
+		System.out.println("Enter double value as input");
+
+		double nextDouble = sc.nextDouble();
+		System.out.println("Output value is" +nextDouble);
+		
+		System.out.println("Enter boolean value as input");
+		boolean nextBoolean = sc.nextBoolean();
+		System.out.println("Output value is" +nextBoolean);
+
+		System.out.println("Enter string value as input");
+
+		String next = sc.next();
+		System.out.println("Output value is" +next);
+
+	}
+	
+
+}
---
+++ src/org/tcs/test/TypesOfData.java
@@ -0,0 +1,38 @@
+package org.tcs.test;
+
+public class TypesOfData {
+	
+	public static void main(String[] args) {
+		
+		//datatype variablename = value
+		byte age = 18;
+		System.out.println("Age is : "+age);
+		
+		short atmPin = 1234;
+		System.out.println("ATM PIN IS : "+atmPin  );
+		
+		int pinCode = 600034;
+		System.out.println("Pin code is "+pinCode);
+		
+		long mobNo = 9342344344l;
+		System.out.println("Mob no is"+mobNo);
+		
+		float weight = 72.53f;
+		System.out.println("Weight is"+weight);
+		
+		double salary = 150000.55;
+		System.out.println("Salary is"+salary);
+		
+		boolean maritalStatus = true;
+		System.out.println("Marital status is"+maritalStatus);
+		
+		char gender = 'M';
+		System.out.println("Gender is" +gender);
+		
+		String name = "Mr.Raj Kumar.C";
+		System.out.println("Name is "+name);
+		
+		
+	}
+
+}
